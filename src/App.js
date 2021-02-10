16333c6a681d8c1560d3d5728f3e5993c09334e0
Ragnar790 <kaucykroy@gmail.com>
2021-02-10 10:12:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import cardImg from "./cards.png";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//STATES
+	const [cardNumber, setCardNumber] = useState();
+	const [expDate, setExpDate] = useState("");
+	const [cvc, setCVC] = useState();
+	const [cardOwner, setCardOwner] = useState("");
+	const [error, setError] = useState("");
+
+	const submitHandler = () => {
+		fetch("http://localhost:8080/confirmPayment", {
+			method: "POST",
+			body: JSON.stringify({ cardNumber, expDate, cvc, cardOwner }),
+			headers: { "Content-Type": "application/json" },
+		})
+			.then((r) => {
+				if (r.ok) {
+					return { success: true };
+				} else {
+					return r.json();
+				}
+			})
+			.then((r) => {
+				if (r.success === true) {
+					window.alert("Signed up!");
+				} else {
+					setError(r.error);
+				}
+			});
+	};
+	return (
+		<div className="App">
+			<h1>Payment Details</h1>
+			<img src={cardImg} alt="cards" />
+			<Form>
+				<FormGroup>
+					<Label for="cardNumber">Card Number</Label>
+					<Input
+						type="number"
+						id="cardNumber"
+						placeholder="Valid Card Number"
+						value={cardNumber}
+						onChange={(e) => setCardNumber(e.target.value)}
+					/>
+				</FormGroup>
+				<FormGroup>
+					<Label for="ExpirationDate">Expiration Date</Label>
+					<Input
+						type="text"
+						id="ExpirationDate"
+						placeholder="MM/YY"
+						value={expDate}
+						onChange={(e) => setExpDate(e.target.value)}
+					/>
+				</FormGroup>
+				<FormGroup>
+					<Label for="CVC">CV Code</Label>
+					<Input
+						type="number"
+						name="email"
+						id="CVC"
+						placeholder="CVC"
+						value={cvc}
+						onChange={(e) => setCVC(e.target.value)}
+					/>
+				</FormGroup>
+				<FormGroup>
+					<Label for="cardOwner">Card Owner</Label>
+					<Input
+						type="email"
+						name="email"
+						id="cardOwner"
+						placeholder="Card Owner Name"
+						value={cardOwner}
+						onChange={(e) => setCardOwner(e.target.value)}
+					/>
+				</FormGroup>
+				<h6 className="error">{error}</h6>
+				<Button color="primary" onClick={submitHandler}>
+					Confirm Payment
+				</Button>
+			</Form>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/backend/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(
+	cors({
+		//because the frontend and backend are running on different servers
+		credentials: true,
+		//frontend path
+		origin: "http://localhost:3000",
+	})
+);
+
+//CREATING THE CONNECTION
+const db = mongoose.createConnection("mongodb://localhost:27017/To-do", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+db.on("connected", () => {
+	console.log("connected to database.");
+});
+
+//DEFINING THE SCHEMA
+const cardDetailsSchema = new mongoose.Schema({
+	cardNumber: Number,
+	expDate: String,
+	cvc: Number,
+	cardOwner: String,
+});
+//Model
+const cardDetailsModel = db.model("cardDetail", cardDetailsSchema);
+
+// function to check null or undefined
+const nullOrUnd = (val) => val === null || val === undefined;
+
+app.post("/confirmPayment", async (req, res) => {
+	const { cardNumber, expDate, cvc, cardOwner } = req.body;
+	console.log(cardNumber, expDate, cvc, cardOwner);
+	if (
+		cardNumber.toString().length === 16 &&
+		!nullOrUnd(expDate) &&
+		!nullOrUnd(cvc) &&
+		!nullOrUnd(cardOwner)
+	) {
+		const newCard = new cardDetailsModel({
+			cardNumber,
+			expDate,
+			cvc,
+			cardOwner,
+		});
+		await newCard.save();
+		res.status(201).send(newCard);
+	} else {
+		console.log(cardNumber.toString().length);
+		res.status(401).send({
+			error:
+				"Please enter a valid card number and ensure  all fields are entered",
+		});
+	}
+});
+
+//Defining the port
+app.listen(8080, () => {
+	console.log("app listening on port 8080");
+});
